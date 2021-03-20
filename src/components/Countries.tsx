45da9214308850ab668fcab0382f708c0e4cc103
Bence Rozsa <robence@gmail.com>
2021-03-20 06:44:18 +0100

    chore: organize folders

---
+++ src/components/Countries.tsx
@@ -0,0 +1,40 @@
+import { useQuery, gql } from '@apollo/client'
+import styles from '../styles/Home.module.css'
+
+const GET_COUNTRY_CODE_AND_NAMES = gql`
+  query GetCountryCodeAndNames {
+    countries {
+      code
+      name
+      emoji
+    }
+  }
+`
+
+export default function Countries() {
+  const { data, loading, error } = useQuery(GET_COUNTRY_CODE_AND_NAMES)
+
+  if (loading) {
+    return <h2>Loading...</h2>
+  }
+
+  if (error) {
+    console.error(error)
+    return null
+  }
+
+  const countries = data.countries.slice(0, 4)
+
+  return (
+    <div className={styles.grid}>
+      {countries.map((country) => (
+        <div key={country.code} className={styles.card}>
+          <h3>{country.name}</h3>
+          <p>
+            {country.code} - {country.emoji}
+          </p>
+        </div>
+      ))}
+    </div>
+  )
+}
